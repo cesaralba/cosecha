2e6221c4956b82f4dd94c369e750ceebd062276c
cap@CASA <cesar.alba@gmail.com>
2024-04-01 22:26:30 +0200

    Backup of state & execution lock

--- scripts/runAll.sh
+++ scripts/runAll.sh
@@ -2,6 +2,16 @@
 
 set -eu
 
+#https://www.putorius.net/lock-files-bash-scripts.html
+LOCKFILE=/var/tmp/actualizaCosecha.lock
+if { set -C; >${LOCKFILE}; }; then
+       trap "rm -f ${LOCKFILE}" EXIT
+else
+       echo "Lock file '${LOCKFILE} exists… exiting"
+       exit
+fi
+
+
 BASEDIR=$(cd "$(dirname $(readlink -e $0))" && pwd )
 
 CONFIGFILE=${DEVSMCONFIGFILE:-/etc/sysconfig/Cosecha}
@@ -10,5 +20,5 @@ CONFIGFILE=${DEVSMCONFIGFILE:-/etc/sysconfig/Cosecha}
 bash ${BASEDIR}/buildDataTree.sh
 bash ${BASEDIR}/buildVENV.sh
 bash ${BASEDIR}/checkScripts.sh || true
-
-bash ${BASEDIR}/downloadCosecha.sh
+bash ${BASEDIR}/backupStates.sh
+bash ${BASEDIR}/downloadCosecha.sh $*
